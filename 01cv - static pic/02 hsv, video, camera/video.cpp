2e266be5b0894007cb18871b5d1a1ccefd64f967
elPytel <jaroslav.korner1@gmail.com>
2024-09-26 11:31:53 +0200

    sledovani kamery

--- 01cv - static pic/02 hsv, video, camera/video.cpp
+++ 01cv - static pic/02 hsv, video, camera/video.cpp
@@ -1,9 +1,25 @@
 // icp.cpp 
 // Author: JJ
 
+// C++ 
+#include <opencv2/opencv.hpp>
+#include <iostream>
+#include <stdexcept>
+#include <ctime>
+
 cv::VideoCapture capture;
 
 //=====================================================================================================
+
+enum Keys {
+    KEY_ESC = 27,
+    KEY_ENTER = 13,
+    KEY_SPACE = 32,
+    KEY_UP = 2490368,
+    KEY_DOWN = 2621440,
+    KEY_LEFT = 2424832,
+    KEY_RIGHT = 2555904
+} my_key_t;
 
 static void init(void)
 {
@@ -23,10 +39,19 @@ static void init(void)
     }
 }
 
-int main(int argc, char * argv[])
+/*
+Find the center of white area in the image.
+*/
+void find_center(cv::Mat& img, cv::Point& center)
 {
-    cv::Mat frame, scene;
-    double h_low = 128.0;
+    cv::Moments m = cv::moments(img, true);
+    center.x = m.m10 / m.m00;
+    center.y = m.m01 / m.m00;
+}
+
+int main(int argc, char * argv[]) {
+    cv::Mat frame, scene, eroded, dilated;
+    double h_low = 150.0;
     double s_low = 128.0;
     double v_low = 128.0;
     double h_hi = 255.0;
@@ -35,19 +60,33 @@ int main(int argc, char * argv[])
 
     init();
 
-    while (1)
-    {
+    // Create a structuring element for dilation and erosion
+    int erosion_size = 2;
+    int dilation_size = 3;
+
+    cv::Mat dilation_element = cv::getStructuringElement(cv::MORPH_RECT,
+                                        cv::Size(2 * dilation_size + 1, 2 * dilation_size + 1),
+                                        cv::Point(dilation_size, dilation_size));
+
+    cv::Mat erosion_element = cv::getStructuringElement(cv::MORPH_RECT,
+                                                cv::Size(2 * erosion_size + 1, 2 * erosion_size + 1),
+                                                cv::Point(erosion_size, erosion_size));
+
+    // green color
+    cv::Scalar green = cv::Scalar(0, 255, 0);
+    int thickness = 2;
+    int rectangle_x_size = 15;
+    int rectangle_y_size = 20;
+
+    while (1) {
         capture.read(frame);
-        if (frame.empty())
-        {
+        if (frame.empty()) {
             std::cerr << "Cam disconnected?" << std::endl;
             break;
         }
 
-        cv::imshow("grabbed", frame);
 
-        // analyze the image...
-        // ...
+        // analyze the image
         cv::Mat scene_hsv, scene_threshold;
 
         cv::cvtColor(frame, scene_hsv, cv::COLOR_BGR2HSV);
@@ -55,11 +94,36 @@ int main(int argc, char * argv[])
         cv::Scalar lower_threshold = cv::Scalar(h_low, s_low, v_low);
         cv::Scalar upper_threshold = cv::Scalar(h_hi, s_hi, v_hi);
         cv::inRange(scene_hsv, lower_threshold, upper_threshold, scene_threshold);
+        //cv::imshow("threshold", scene_threshold);
 
-        cv::imshow("threshold", scene_threshold);
+        // Apply erosion
+        cv::erode(scene_threshold, eroded, erosion_element);
 
-        if (cv::waitKey(1) == 27)
+        // Apply dilation
+        cv::dilate(eroded, dilated, dilation_element);
+
+        // find center
+        cv::Point center;
+        find_center(dilated, center);
+        std::cout << "Center absolute: " << center << '\n';
+        // add rectangle
+        cv::Point left_top(center.x - rectangle_x_size, center.y - rectangle_y_size);
+        cv::Point right_bottom(center.x + rectangle_x_size, center.y + rectangle_y_size);
+        cv::rectangle(frame, left_top, right_bottom, green, thickness);
+
+        // Display the resulting frame
+        cv::imshow("grabbed", frame);
+        cv::imshow("threshold", dilated);
+
+        if (cv::waitKey(1) == KEY_ESC) {
             break;
+        }
+        
+        // wait 1s
+        int wait_time = 500;
+        if (cv::waitKey(wait_time) == KEY_RIGHT) {
+            continue;
+        }
     }
 
     if (capture.isOpened())
